c011d2a3cc5cfc0f8a392f2b532b763190c831ad
Silur <abrahamendre9@gmail.com>
2020-06-18 16:38:14 +0200

    heavy refactoring

--- README.md
+++ README.md
@@ -15,33 +15,18 @@ The parties agree on a pseudorandom function (SHA3) and exchange a proof with th
 ## Usage
 
 ```rust
-    extern crate rand_os;
-    extern crate ec_vrf;
-    use curve25519_dalek::scalar::{Scalar};
-    use curve25519_dalek::constants::ED25519_BASEPOINT_POINT as g;
-    use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT as rg;
-    use rand_os::OsRng;
-
-   
-    fn main() {
-        let mut csprng: OsRng = OsRng::new().unwrap();
-        let privkey: Scalar = Scalar::random(&mut csprng);
-        let input = vec![1,2,3,4,5,6,7,8];
-		// using curve25519
-        let pubkey = g*privkey;
-        let (output, proof) = ec_vrf::curve25519::prove(&input, privkey);
-        assert!(ec_vrf::curve25519::verify(&input, pubkey, output, proof));
-
-		// using ristretto
-        let pubkey2 = rg*privkey;
-        let (output2, proof2) = ec_vrf::ristretto::prove(&input, privkey);
-        assert!(ec_vrf::ristretto::verify(&input, pubkey2, output2, proof2));
-    }
+use ecvrf::{VrfSk, VrfPk, VrfProof, keygen, prove, verify};
+fn main() {
+	let (privkey, pubkey): VrfSk = keygen()
+						   let input = vec![0xde, 0xad, 0xbe, 0xef];
+	let (output, proof) = ecvrf::prove(&input, privkey);
+	assert!(ecvrf::verify(&input, &pubkey, output, proof));
+}
 }
 
 ```
 
-
+You can serialize/deserialize each type with the corresponding `to_bytes` and `from_bytes` functions
 
 ## Disclaimer
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
-pub use rand_os::OsRng;
-pub use sha3::{Digest, Sha3_256 as SHA3};
-pub use curve25519_dalek::scalar::Scalar;
-pub use curve25519_dalek::constants::ED25519_BASEPOINT_POINT;
-pub use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
-pub use curve25519_dalek::ristretto::RistrettoPoint;
-pub use curve25519_dalek::edwards::EdwardsPoint;
+use rand_core::{OsRng};
+use sha3::{Digest, Sha3_256 as SHA3};
+use curve25519_dalek::scalar::Scalar;
+use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT as g;
+use curve25519_dalek::ristretto::{RistrettoPoint, CompressedRistretto};
 
-pub fn sha3(b: Vec<u8>) -> [u8; 32] {
+fn sha3(b: Vec<u8>) -> [u8; 32] {
     let mut hasher = SHA3::default();
     hasher.input(b);
     let r = hasher.result();
@@ -17,195 +15,184 @@ pub fn sha3(b: Vec<u8>) -> [u8; 32] {
     ret
 }
 
-pub mod curve25519 {
-    pub use super::*;
-    pub use super::ED25519_BASEPOINT_POINT as g;
-    pub type EcPoint = EdwardsPoint;
-    pub struct VrfProof {
-        gamma: EcPoint,
-        c: [u8; 32],
-        s: Scalar,
-    }
-    fn hash_to_point(b: Vec<u8>) -> EcPoint {
-        let hash = sha3(b);
-        let s = Scalar::from_bytes_mod_order(hash);
-        g * s
-    }
-    fn serialize_point(p: EcPoint) -> [u8; 32] {
-        p.to_montgomery().to_bytes()
-    }
-    pub fn prove(input: &[u8], privkey: &Scalar) -> ([u8; 32], VrfProof) {
-        let h = hash_to_point(input.to_vec());
-        let gamma = h * privkey;
-        let mut csprng: OsRng = OsRng::new().unwrap();
-        let k: Scalar = Scalar::random(&mut csprng);
-        let mut hasher = SHA3::default();
-        hasher.input(serialize_point(g));
-        hasher.input(serialize_point(h));
-        hasher.input(serialize_point(g * privkey));
-        hasher.input(serialize_point(h * privkey));
-        hasher.input(serialize_point(g * k));
-        hasher.input(serialize_point(h * k));
-        let mut c = [0 as u8; 32];
-        let hres = hasher.result();
-        for i in 0..hres.len() {
-            c[i] = hres[i];
-        }
-        let c_scalar = Scalar::from_bytes_mod_order(c);
-        let s = k - c_scalar * privkey;
-        let beta = sha3(serialize_point(gamma).to_vec());
-        (beta, VrfProof { gamma, c, s })
-    }
-
-    pub fn verify(input: &[u8], pubkey: &EcPoint, output: &[u8; 32], proof: &VrfProof) -> bool {
-        let c_scalar = Scalar::from_bytes_mod_order(proof.c);
-        let u = pubkey * c_scalar + g * proof.s;
-        let h = hash_to_point(input.to_vec());
-        let v = proof.gamma * c_scalar + h * proof.s;
-
-        let mut hasher = SHA3::default();
-        hasher.input(serialize_point(g));
-        hasher.input(serialize_point(h));
-        hasher.input(serialize_point(*pubkey));
-        hasher.input(serialize_point(proof.gamma));
-        hasher.input(serialize_point(u));
-        hasher.input(serialize_point(v));
-        let mut local_c = [0 as u8; 32];
-        let hres = hasher.result();
-        for i in 0..hres.len() {
-            local_c[i] = hres[i];
-        }
-        sha3(serialize_point(proof.gamma).to_vec()) == *output && local_c == proof.c
-    }
+#[derive(Debug, PartialEq, Eq)]
+/// Holds a secret key scalar for generating VRF proofs
+pub struct VrfSk {
+    s: Scalar
 }
-pub mod ristretto {
-    pub use super::*;
-    pub use super::RISTRETTO_BASEPOINT_POINT as g;
-    pub use curve25519_dalek::ristretto::CompressedRistretto;
-    pub type EcPoint = RistrettoPoint;
-    pub struct VrfProof {
-        gamma: EcPoint,
-        c: [u8; 32],
-        s: Scalar,
+
+#[derive(Debug, PartialEq, Eq)]
+/// Holds a public key g*sk as usual in ECC
+pub struct VrfPk {
+    p: RistrettoPoint
+}
+
+#[derive(Debug, PartialEq, Eq)]
+/// The proof part of our VRF generation
+pub struct VrfProof {
+    gamma: RistrettoPoint,
+    c: [u8; 32],
+    s: Scalar,
+}
+
+impl VrfSk {
+    pub fn new() -> VrfSk {
+        return VrfSk { s: Scalar::random(&mut OsRng) };
     }
-    impl VrfProof{
-        pub fn to_bytes(&self) -> [u8;96]{
-            let mut ret = [0;96];
-            let gamma = self.gamma.compress().to_bytes();
-            let c = self.c;
-            let s = self.s.to_bytes();
-            for i in 0..31{
-                ret[i]=gamma[i];
-                ret[32+i]=c[i];
-                ret[64+i]=s[i];
-            }
-            ret
-        }
-
-        pub fn from_slices(gamma : [u8;32], c : [u8;32], s : [u8;32]) -> VrfProof{
-            VrfProof{
-                gamma : CompressedRistretto::from_slice(&gamma).decompress().expect("invalid compressed RisrettoPoint"),
-                c : c,
-                s : Scalar::from_bytes_mod_order(s)
-            }
-        }
-
-        pub fn from_bytes(input : [u8;96]) -> VrfProof{
-            let mut gamma : [u8;32]=[0;32];
-            let mut c : [u8;32]=[0;32];
-            let mut s : [u8;32]=[0;32];
-            gamma.copy_from_slice(&input[0..31]); 
-            c.copy_from_slice(&input[32..63]); 
-            s.copy_from_slice(&input[64..95]); 
-            VrfProof::from_slices(gamma,c,s)
-        }
-
+    pub fn to_bytes(&self) -> [u8; 32] {
+        return self.s.to_bytes();
     }
-
-    fn hash_to_point(b: Vec<u8>) -> EcPoint {
-        let hash = sha3(b);
-        let s = Scalar::from_bytes_mod_order(hash);
-        g * s
-    }
-    fn serialize_point(p: EcPoint) -> [u8; 32] {
-        p.compress().to_bytes()
-    }
-    pub fn prove(input: &[u8], privkey: &Scalar) -> ([u8; 32], VrfProof) {
-        let privkey = privkey.clone();
-        let h = hash_to_point(input.to_vec());
-        let gamma = h * privkey;
-        let mut csprng: OsRng = OsRng::new().unwrap();
-        let k: Scalar = Scalar::random(&mut csprng);
-        let mut hasher = SHA3::default();
-        hasher.input(serialize_point(g));
-        hasher.input(serialize_point(h));
-        hasher.input(serialize_point(g * privkey));
-        hasher.input(serialize_point(h * privkey));
-        hasher.input(serialize_point(g * k));
-        hasher.input(serialize_point(h * k));
-        let mut c = [0 as u8; 32];
-        let hres = hasher.result();
-        for i in 0..hres.len() {
-            c[i] = hres[i];
-        }
-        let c_scalar = Scalar::from_bytes_mod_order(c);
-        let s = k - c_scalar * privkey;
-        let beta = sha3(serialize_point(gamma).to_vec());
-        (beta, VrfProof { gamma, c, s })
-    }
-
-    pub fn verify(input: &[u8], pubkey: &EcPoint, output: &[u8; 32], proof: &VrfProof) -> bool {
-        let c_scalar = Scalar::from_bytes_mod_order(proof.c);
-        let u = pubkey * c_scalar + g * proof.s;
-        let h = hash_to_point(input.to_vec());
-        let v = proof.gamma * c_scalar + h * proof.s;
-
-        let mut hasher = SHA3::default();
-        hasher.input(serialize_point(g));
-        hasher.input(serialize_point(h));
-        hasher.input(serialize_point(*pubkey));
-        hasher.input(serialize_point(proof.gamma));
-        hasher.input(serialize_point(u));
-        hasher.input(serialize_point(v));
-        let mut local_c = [0 as u8; 32];
-        let hres = hasher.result();
-        for i in 0..hres.len() {
-            local_c[i] = hres[i];
-        }
-        sha3(serialize_point(proof.gamma).to_vec()) == *output && local_c == proof.c
+    pub fn from_bytes(b: &[u8; 32]) -> Result<VrfSk, &str> {
+        let s = Scalar::from_canonical_bytes(*b);
+        if s.is_none() { return Err("private key is not canonical"); }
+        return Ok(VrfSk{ s: s.unwrap() });
     }
 }
 
+impl VrfPk {
+    pub fn new(sk: &VrfSk) -> VrfPk {
+        return VrfPk { p: g*sk.s };
+    }
+    pub fn to_bytes(&self) -> [u8; 32] {
+        return self.p.compress().to_bytes();
+    }
+    pub fn from_bytes(b: &[u8; 32]) -> Result<VrfPk, &str> {
+        let p = CompressedRistretto::from_slice(b).decompress();
+        if p.is_none() { return Err("public key is not canonical"); }
+        return Ok(VrfPk{ p: p.unwrap() });
+    }
+}
 
+impl VrfProof {
+    pub fn to_bytes(&self) -> [u8;96]{
+        let mut ret = [0;96];
+        let gamma = self.gamma.compress().to_bytes();
+        let c = self.c;
+        let s = self.s.to_bytes();
+        for i in 0..32 {
+            ret[i]=gamma[i];
+            ret[32+i]=c[i];
+            ret[64+i]=s[i];
+        }
+        ret
+    }
+    pub fn from_bytes(input: &[u8;96]) -> Result<VrfProof, &str> {
+        let mut gamma : [u8;32]=[0;32];
+        let mut c : [u8;32]=[0;32];
+        let mut s : [u8;32]=[0;32];
+        gamma.copy_from_slice(&input[0..32]); 
+        c.copy_from_slice(&input[32..64]); 
+        s.copy_from_slice(&input[64..96]);
+        let gamma_point = CompressedRistretto::from_slice(&gamma).decompress();
+        let s_reduced = Scalar::from_canonical_bytes(s);
+        if gamma_point.is_none() || s_reduced.is_none() { return Err("cannot deserialize vrf proof"); }
+        return Ok(VrfProof {
+            gamma: gamma_point.unwrap(),
+            c: c,
+            s: s_reduced.unwrap()
+        });
+    }
+}
+
+fn hash_to_point(b: Vec<u8>) -> RistrettoPoint {
+    let hash = sha3(b);
+    let s = Scalar::from_bytes_mod_order(hash);
+    g * s
+}
+fn serialize_point(p: RistrettoPoint) -> [u8; 32] {
+    p.compress().to_bytes()
+}
+
+/// Generates a crypto-safe secret key using OsRng and the
+/// corresponding public key into a tuple
+pub fn keygen() -> (VrfSk, VrfPk) {
+    let sk = VrfSk::new();
+    let pk = VrfPk::new(&sk);
+    return (sk, pk);
+}
+/// The output of a VRF function is the VRF hash and the proof to verify
+/// we generated this hash with the supplied key
+pub fn prove(input: &[u8], privkey: &VrfSk) -> ([u8; 32], VrfProof) {
+    let h = hash_to_point(input.to_vec());
+    let gamma = h * privkey.s;
+    let k: Scalar = Scalar::random(&mut OsRng);
+    let mut hasher = SHA3::default();
+    hasher.input(serialize_point(g));
+    hasher.input(serialize_point(h));
+    hasher.input(serialize_point(g * privkey.s));
+    hasher.input(serialize_point(h * privkey.s));
+    hasher.input(serialize_point(g * k));
+    hasher.input(serialize_point(h * k));
+    let mut c = [0 as u8; 32];
+    let hres = hasher.result();
+    for i in 0..hres.len() {
+        c[i] = hres[i];
+    }
+    let c_scalar = Scalar::from_bytes_mod_order(c);
+    let s = k - c_scalar * privkey.s;
+    let beta = sha3(serialize_point(gamma).to_vec());
+    (beta, VrfProof { gamma, c, s })
+}
+
+pub fn verify(input: &[u8], pubkey: &VrfPk, output: &[u8; 32], proof: &VrfProof) -> bool {
+    let c_scalar = Scalar::from_bytes_mod_order(proof.c);
+    let u = pubkey.p * c_scalar + g * proof.s;
+    let h = hash_to_point(input.to_vec());
+    let v = proof.gamma * c_scalar + h * proof.s;
+
+    let mut hasher = SHA3::default();
+    hasher.input(serialize_point(g));
+    hasher.input(serialize_point(h));
+    hasher.input(serialize_point(pubkey.p));
+    hasher.input(serialize_point(proof.gamma));
+    hasher.input(serialize_point(u));
+    hasher.input(serialize_point(v));
+    let mut local_c = [0 as u8; 32];
+    let hres = hasher.result();
+    for i in 0..hres.len() {
+        local_c[i] = hres[i];
+    }
+    sha3(serialize_point(proof.gamma).to_vec()) == *output && local_c == proof.c
+}
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use super::*;
-    use curve25519_dalek::constants::ED25519_BASEPOINT_POINT as g;
-    use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT as rg;
-    use curve25519_dalek::scalar::Scalar;
-    use rand_os::OsRng;
+    #[test]
+    fn correct_proof() {
+        let (privkey, pubkey) = super::keygen();
+        let input = vec![0xde, 0xad, 0xbe, 0xef];
+        let (output, proof) = super::prove(&input, &privkey);
+
+        assert!(super::verify(&input, &pubkey, &output, &proof));
+
+    }
+    #[test]
+    fn serialize() {
+        let (privkey, pubkey) = super::keygen();
+        let input = vec![0xde, 0xad, 0xbe, 0xef];
+        let (_, proof) = super::prove(&input, &privkey);
+        let sk_serialized = privkey.to_bytes();
+        let pk_serialized = pubkey.to_bytes();
+        let proof_serialized = proof.to_bytes();
+        assert_eq!(super::VrfSk::from_bytes(&sk_serialized).unwrap(), privkey);
+        assert_eq!(super::VrfPk::from_bytes(&pk_serialized).unwrap(), pubkey);
+        assert_eq!(super::VrfProof::from_bytes(&proof_serialized).unwrap(), proof);
+    }
 
     #[test]
-    fn curve25519() {
-        let mut csprng: OsRng = OsRng::new().unwrap();
-        let privkey: Scalar = Scalar::random(&mut csprng);
-        let pubkey = g * privkey;
-        let input = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        let (output, proof) = curve25519::prove(&input, &privkey);
-        assert!(curve25519::verify(&input, &pubkey, &output, &proof));
-    }
- /*   #[bench]
-    fn bench_curve25519(b: &mut Bencher) {
-        b.iter(|| curve25519());
-    } */
-    #[test]
-    fn ristretto() {
-        let mut csprng: OsRng = OsRng::new().unwrap();
-        let privkey: Scalar = Scalar::random(&mut csprng);
-        let pubkey = rg * privkey;
-        let input = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        let (output, proof) = ristretto::prove(&input, &privkey);
-        assert!(ristretto::verify(&input, &pubkey, &output, &proof));
+    fn forgery() {
+        let (privkey, pubkey) = super::keygen();
+        let (forge_privkey, forge_pubkey) = super::keygen();
+        let input = vec![0xde, 0xad, 0xbe, 0xef];
+        let input_forged = vec![0xde, 0xad, 0xbe, 0xed];
+        let (output, proof) = super::prove(&input, &privkey);
+        let (forge_output, forge_proof) = super::prove(&input, &forge_privkey);
+        let mut output_forged = output.clone();
+        output_forged[0] += 0x01;
+
+        assert!(!super::verify(&input_forged, &pubkey, &output, &proof));
+        assert!(!super::verify(&input, &pubkey, &output_forged, &proof));
+        assert!(!super::verify(&input, &pubkey, &forge_output, &proof));
+        assert!(!super::verify(&input, &pubkey, &output, &forge_proof));
+        assert!(!super::verify(&input, &forge_pubkey, &output, &proof));
     }
 }
